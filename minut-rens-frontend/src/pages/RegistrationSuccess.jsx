88ecba1e4a52cf74440bd6f24f303b39e2b91ab9
Ali Raza Akhtar <alirazaakhtar@MacBookAir.home>
2025-06-30 17:09:10 +0200

    register success

--- minut-rens-frontend/src/pages/RegistrationSuccess.jsx
+++ minut-rens-frontend/src/pages/RegistrationSuccess.jsx
@@ -6,7 +6,7 @@ const RegistrationSuccess = () => {
       <div className="text-center px-3">
         <h1 className="display-5 mb-4">Velkommen ombord!</h1>
         <p className="lead mb-4">Din konto er blevet oprettet succesfuldt. Du kan nu logge ind og komme i gang.</p>
-        <Link to="login" className="btn btn-success btn-lg w-100">Gå til login</Link>
+        <Link to="/login" className="btn btn-success btn-lg w-100">Gå til login</Link>
       </div>
     </div>
   );
